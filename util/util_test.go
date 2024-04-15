1984b888850d84f8bdf0ab494f911f7c64f0e24a
Dom <peluso.dominick@gmail.com>
2024-04-15 14:30:06 -0400

    Complete pad right tests

--- util/util_test.go
+++ util/util_test.go
@@ -14,25 +14,24 @@ func TestPadRight(t *testing.T) {
 	}{
 		{
 			name: "TestPadRight",
-			args: args{
-				str:    "hello",
-				length: 10,
-			},
+			args: args{str: "hello", length: 10},
 			want: "hello     ",
 		},
 		{
 			name: "TestLongerInput",
-			args: args{
-				str:    "hello world",
-				length: 5,
-			},
-			want: "hello world ",
+			args: args{str: "hello world", length: 5},
+			want: "hello world",
+		},
+		{
+			name: "TestEmpty",
+			args: args{str: "", length: 5},
+			want: "     ",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := PadRight(tt.args.str, tt.args.length); got != tt.want {
-				t.Errorf("PadRight() = %v, want %v", got, tt.want)
+				t.Errorf("PadRight() = '%v', want '%v'", got, tt.want)
 			}
 		})
 	}
