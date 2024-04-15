d2640d0febd73e13fd7c12eb1b3d1a7889b9fdf8
Dom <peluso.dominick@gmail.com>
2024-04-15 16:01:25 -0400

    Update model_test.go

--- model_test.go
+++ model_test.go
@@ -52,3 +52,82 @@ func Test_getTopMenuCurrentPageChoices(t *testing.T) {
 		})
 	}
 }
+
+func Test_model_getCurrentTopic(t *testing.T) {
+	tests := []struct {
+		name string
+		m    model
+		want *client.Item
+	}{
+		{
+			name: "TestGetCurrentTopic",
+			m: model{topicHistoryStack: []client.Item{
+				{Id: 1, Title: "item 1"},
+				{Id: 1, Title: "item 2"},
+				{Id: 1, Title: "item 3"},
+			}},
+			want: &client.Item{Id: 1, Title: "item 3"},
+		},
+		{
+			name: "TestGetCurrentTopicEmptyStack",
+			m:    model{topicHistoryStack: []client.Item{}},
+			want: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.m.getCurrentTopic(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("model.getCurrentTopic() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_getContent(t *testing.T) {
+	type args struct {
+		m model
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "TestGetContentWithCurrentTopic",
+			args: args{m: model{
+				topicHistoryStack: []client.Item{{
+					Id: 1, Title: "item 1", By: "Joe", Kids: []int{1, 2, 3}, Url: "http://example.com", Text: "foo...",
+				}},
+				choices: []string{"33   item 1", "33   item 2"},
+			}},
+			want: `item 1
+By Joe (3 comments)
+    foo...
+→ http://example.com
+
+> 33   item 1
+  33   item 2
+`,
+		},
+		{
+			name: "TestGetContentTopMenu",
+			args: args{m: model{
+				topicHistoryStack: []client.Item{},
+				choices:           []string{"33   item 1", "33   item 2", "33   item 3", "33   item 4", "33   item 5"},
+			}},
+			want: `> 33   item 1
+  33   item 2
+  33   item 3
+  33   item 4
+  33   item 5
+`,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getContent(tt.args.m); got != tt.want {
+				t.Errorf("getContent() = \n'%v', want \n'%v'", got, tt.want)
+			}
+		})
+	}
+}
